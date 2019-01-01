9141b6d7e2555f277611fc2eb6c71d8ba16cfb5e
mes32 <stockman.mike@gmail.com>
2019-01-01 15:36:19 -0600

    Animate 'hello world' bouncing around the game field.

--- project/scripts/game.js
+++ project/scripts/game.js
@@ -1,8 +1,42 @@
-function startGame(ctx) {
-    console.log('startGame()');
+function startGame(canvas) {
+    const UPDATE_INTERVAL = 40;
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
 
-    ctx.fillStyle = 'darkslategrey';
-    ctx.font = '30px Courier New';
+    const context = canvas.getContext('2d');
+    context.fillStyle = 'darkslategrey';
+    context.font = '30px Courier New';
 
-    ctx.fillText('Hello world!', 50, 80);
+    let x = 50;
+    let y = 50;
+    let moveX = 10;
+    let moveY = -10;
+
+    setInterval(updateGame, UPDATE_INTERVAL);
+    function updateGame() {
+        context.clearRect(x, y - 30, 300, 40);
+        x += moveX;
+        y += moveY;
+        context.fillText('Hello world!', x, y);
+
+        if (x <= 0 || x + 200 >= WIDTH) {
+            moveX *= -1;
+        }
+        if (y - 30 <= 0 || y >= HEIGHT) {
+            moveY *= -1;
+        }
+    }
+
+    // let centerX = canvas.width / 2;
+    // let centerY = canvas.height / 2;
+    // let radius = 70;
+
+    // context.beginPath();
+    // context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    // context.fillStyle = 'green';
+    // context.fill();
+    // context.lineWidth = 5;
+    // context.strokeStyle = '#003300';
+    // context.stroke();
+
 }
