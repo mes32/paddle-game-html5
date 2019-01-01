b424c8b5cead0c426576183874343a5370c3d646
mes32 <stockman.mike@gmail.com>
2019-01-01 14:41:17 -0600

    Print 'hello world' to the canvas element.

---
+++ project/scripts/game.js
@@ -0,0 +1,8 @@
+function startGame(ctx) {
+    console.log('startGame()');
+
+    ctx.fillStyle = 'darkslategrey';
+    ctx.font = '30px Courier New';
+
+    ctx.fillText('Hello world!', 50, 80);
+}
